1d110bdbafa30c4a3e6fde2cdf5fc2b3db72cd51
Andrew Chen <achen4824@gmail.com>
2022-02-02 23:46:10 +1030

    URL verified to be working with rtsp and http using http as real-time stream is unecessary

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # YOLO3 (Detection for Home Camera)
 Forked from experiencor 
 
+```
+python predict.py -c config.json -i /path/to/image/or/video
+```
--- predict.py
+++ predict.py
@@ -1,9 +1,13 @@
 #! /usr/bin/env python
-
 import os
+import time
 import argparse
 import json
+import io
+import imageio
 import cv2
+import requests
+from requests.auth import HTTPDigestAuth
 from utils.utils import get_yolo_boxes, makedirs
 from utils.bbox import draw_boxes
 from keras.models import load_model
@@ -24,25 +28,31 @@ def _main_(args):
     #   Set some parameter
     ###############################       
     net_h, net_w = 416, 416 # a multiple of 32, the smaller the faster
-    obj_thresh, nms_thresh = 0.5, 0.45
+    obj_thresh, nms_thresh = 0.9, 0.45
 
     ###############################
     #   Load the model
     ###############################
     os.environ['CUDA_VISIBLE_DEVICES'] = config['train']['gpus']
-    infer_model = load_model(config['train']['saved_weights_name'])
+    infer_model = load_model(config['train']['saved_weights_name'], compile=False)
 
     ###############################
     #   Predict bounding boxes 
     ###############################
-    if 'webcam' in input_path: # do detection on the first webcam
-        video_reader = cv2.VideoCapture(0)
+    if 'rtsp' in input_path: # do detection on the first webcam
+        video_reader = cv2.VideoCapture(input_path)
+        # video_reader.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
+        # video_reader.set(cv2.CAP_PROP_FRAME_HEIGHT, 512)
+        video_reader.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
         # the main loop
         batch_size  = 1
         images      = []
         while True:
             ret_val, image = video_reader.read()
+
+            image = cv2.resize(image, (960, 512))
+
             if ret_val == True: images += [image]
 
             if (len(images)==batch_size) or (ret_val==False and len(images)>0):
@@ -54,74 +64,38 @@ def _main_(args):
                 images = []
             if cv2.waitKey(1) == 27: 
                 break  # esc to quit
-        cv2.destroyAllWindows()        
-    elif input_path[-4:] == '.mp4': # do detection on a video  
-        video_out = output_path + input_path.split('/')[-1]
-        video_reader = cv2.VideoCapture(input_path)
+        cv2.destroyAllWindows() 
+    elif 'http' in input_path:
 
-        nb_frames = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
-        frame_h = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frame_w = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
-
-        video_writer = cv2.VideoWriter(video_out,
-                               cv2.VideoWriter_fourcc(*'MPEG'), 
-                               50.0, 
-                               (frame_w, frame_h))
         # the main loop
         batch_size  = 1
         images      = []
-        start_point = 0 #%
-        show_window = False
-        for i in tqdm(range(nb_frames)):
-            _, image = video_reader.read()
 
-            if (float(i+1)/nb_frames) > start_point/100.:
-                images += [image]
+        session = requests.Session()
+        session.auth = HTTPDigestAuth(username, password)
 
-                if (i%batch_size == 0) or (i == (nb_frames-1) and len(images) > 0):
-                    # predict the bounding boxes
-                    batch_boxes = get_yolo_boxes(infer_model, images, net_h, net_w, config['model']['anchors'], obj_thresh, nms_thresh)
 
-                    for i in range(len(images)):
-                        # draw bounding boxes on the image using labels
-                        draw_boxes(images[i], batch_boxes[i], config['model']['labels'], obj_thresh)   
+        while True:
+            response = session.get(input_path)
+            arr = imageio.imread(io.BytesIO(response.content))
 
-                        # show the video with detection bounding boxes          
-                        if show_window: cv2.imshow('video with bboxes', images[i])  
+            image = cv2.resize(arr, (1920, 1080))
 
-                        # write result to the output video
-                        video_writer.write(images[i]) 
-                    images = []
-                if show_window and cv2.waitKey(1) == 27: break  # esc to quit
+            images += [image]
 
-        if show_window: cv2.destroyAllWindows()
-        video_reader.release()
-        video_writer.release()       
-    else: # do detection on an image or a set of images
-        image_paths = []
+            if (len(images)==batch_size) or (len(images)>0):
+                batch_boxes = get_yolo_boxes(infer_model, images, net_h, net_w, config['model']['anchors'], obj_thresh, nms_thresh)
 
-        if os.path.isdir(input_path): 
-            for inp_file in os.listdir(input_path):
-                image_paths += [input_path + inp_file]
-        else:
-            image_paths += [input_path]
+                for i in range(len(images)):
+                    draw_boxes(images[i], batch_boxes[i], config['model']['labels'], obj_thresh) 
+                    cv2.imshow('video with bboxes', images[i])
+                images = []
+            if cv2.waitKey(1) == 27: 
+                break  # esc to quit
+            time.sleep(0.2)
 
-        image_paths = [inp_file for inp_file in image_paths if (inp_file[-4:] in ['.jpg', '.png', 'JPEG'])]
 
-        # the main loop
-        for image_path in image_paths:
-            image = cv2.imread(image_path)
-            print(image_path)
-
-            # predict the bounding boxes
-            boxes = get_yolo_boxes(infer_model, [image], net_h, net_w, config['model']['anchors'], obj_thresh, nms_thresh)[0]
-
-            # draw bounding boxes on the image using labels
-            draw_boxes(image, boxes, config['model']['labels'], obj_thresh) 
-     
-            # write the image with bounding boxes to file
-            cv2.imwrite(output_path + image_path.split('/')[-1], np.uint8(image))         
-
+        
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description='Predict with a trained yolo model')
     argparser.add_argument('-c', '--conf', help='path to configuration file')
